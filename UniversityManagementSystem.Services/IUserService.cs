5b46c6fd5884d47f38f3135f3d96da84cd8c8de1
Thomas Clark <tnc1997@virginmedia.com>
2019-02-15 13:18:56 +0000

    feat: add method to get user by credentials

--- UniversityManagementSystem.Services/IUserService.cs
+++ UniversityManagementSystem.Services/IUserService.cs
@@ -1,8 +1,10 @@
+using System.Threading.Tasks;
 using UniversityManagementSystem.Data.Entities;
 
 namespace UniversityManagementSystem.Services
 {
     public interface IUserService : IService<User>
     {
+        Task<User> GetAsync(string username, string password);
     }
 }
--- UniversityManagementSystem.Services/UserService.cs
+++ UniversityManagementSystem.Services/UserService.cs
@@ -1,6 +1,8 @@
 using System.Data.Entity;
+using System.Threading.Tasks;
 using UniversityManagementSystem.Data.Contexts;
 using UniversityManagementSystem.Data.Entities;
+using static BCrypt.Net.BCrypt;
 
 namespace UniversityManagementSystem.Services
 {
@@ -9,6 +11,16 @@ namespace UniversityManagementSystem.Services
         protected override DbSet<User> GetDbSet(ApplicationDbContext context)
         {
             return context.Users;
+        }
+
+        public async Task<User> GetAsync(string username, string password)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var user = await GetDbSet(context).SingleOrDefaultAsync(user1 => user1.UserName == username);
+
+                return user == null || !Verify(password, user.PasswordHash) ? null : user;
+            }
         }
     }
 }
