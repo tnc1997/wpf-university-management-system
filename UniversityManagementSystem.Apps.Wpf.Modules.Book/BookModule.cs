799390d32a112b75708f88ee64c5a4fcc5740c22
Thomas Clark <tnc1997@virginmedia.com>
2019-02-19 08:09:18 +0000

    feat: add book module

---
+++ UniversityManagementSystem.Apps.Wpf.Modules.Book/BookModule.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using UniversityManagementSystem.Apps.Wpf.Modules.Book.ViewModels;
+using UniversityManagementSystem.Apps.Wpf.Modules.Book.Views;
+
+namespace UniversityManagementSystem.Apps.Wpf.Modules.Book
+{
+    public class BookModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+        
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<BooksView, BooksViewModel>();
+        }
+    }
+}
--- UniversityManagementSystem.Apps.Wpf/App.xaml.cs
+++ UniversityManagementSystem.Apps.Wpf/App.xaml.cs
@@ -2,6 +2,7 @@
 using Prism.Ioc;
 using Prism.Modularity;
 using UniversityManagementSystem.Apps.Wpf.Modules.Auth;
+using UniversityManagementSystem.Apps.Wpf.Modules.Book;
 using UniversityManagementSystem.Apps.Wpf.Modules.Graduation;
 using UniversityManagementSystem.Apps.Wpf.Modules.Home;
 using UniversityManagementSystem.Apps.Wpf.Modules.Lecture;
@@ -21,6 +22,7 @@ namespace UniversityManagementSystem.Apps.Wpf
         protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
         {
             moduleCatalog.AddModule<AuthModule>();
+            moduleCatalog.AddModule<BookModule>();
             moduleCatalog.AddModule<GraduationModule>();
             moduleCatalog.AddModule<HomeModule>();
             moduleCatalog.AddModule<LectureModule>();
